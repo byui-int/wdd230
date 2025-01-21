e0e614347ea26d8cb9fb97eebaac56b52e726f99
byui-int <intelligent380@gmail.com>
2025-01-21 14:31:18 +0100

    Update

---
+++ week02/scripts/bom.js
@@ -0,0 +1,4 @@
+// Declare references to the input, button, and list elements
+const input = document.querySelector("#favchap"); // Selects the input field
+const button = document.querySelector("button"); // Selects the button element
+const list = document.querySelector("#list"); // Selects the list (ul) element
